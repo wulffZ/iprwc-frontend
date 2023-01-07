03511b7c7ee294a828982ac9945902d0dabd0f23
Matthijs van Zetten <mattje74@gmail.com>
2023-01-07 01:22:07 +0100

    Changed envirement.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrlAuth: 'http://83.83.127.105:8080/api/auth',
-  apiUrl: 'http://83.83.127.105:8080/api/'
+  apiUrl: 'http://83.83.127.105:8080/api'
 };
 
 /*
