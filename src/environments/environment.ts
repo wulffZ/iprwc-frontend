e890c523fecc38a43b05a6dfdbd182b4b381eb75
Matthijs van Zetten <mattje74@gmail.com>
2023-01-07 01:15:29 +0100

    Adjusted global path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://83.83.127.105:8080/api/auth'
+  apiUrlAuth: 'http://83.83.127.105:8080/api/auth',
+  apiUrl: 'http://83.83.127.105:8080/api/'
 };
 
 /*
